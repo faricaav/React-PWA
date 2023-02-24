3aee55230f21ac4e1900f203ce8e4a4fc0527fcc
faricaav <100252923+faricaav@users.noreply.github.com>
2023-02-24 23:03:20 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import './App.css';
+import { Navbar, Nav } from "react-bootstrap";
+import { Link, Route, BrowserRouter, Switch } from "react-router-dom";
+import About from "./components/about";
+import Home from "./components/home";
+import Users from "./components/users";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/about">About</Link></Nav.Link>
+            <Nav.Link><Link to="/users" >Users</Link></Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/users" component={Users}></Route>
+        </Switch>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
